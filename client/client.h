d6cdc365bccd1643456f6f9192d84cf39e595c88
Brieuc <brieuc.delafouchardiere@epitech.eu>
2014-06-14 15:47:31 +0200

    del header

--- client/client.h
+++ client/client.h
@@ -1,13 +1,3 @@
-/*
-** client.h for client in /home/delafob/delafo_b/SVN/minitalk
-** 
-** Made by Brieuc de La Fouchardière
-** Login   <delafob@epitech.net>
-** 
-** Started on  Mon Mar 11 13:23:46 2013 Brieuc de La Fouchardière
-** Last update Sun Mar 24 17:48:58 2013 Brieuc de La Fouchardière
-*/
-
 #ifndef CLIENT_H_
 # define CLIENT_H_
 
