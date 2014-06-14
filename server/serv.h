a55ef5954b73341e1c508a71237d0155b20ed037
Brieuc <brieuc.delafouchardiere@epitech.eu>
2014-06-14 15:49:15 +0200

    Update serv.h

--- server/serv.h
+++ server/serv.h
@@ -1,13 +1,3 @@
-/*
-** serv.h for serv in /home/delafob/delafo_b/SVN/minitalk-2017-delafo_b
-** 
-** Made by Brieuc de La Fouchardière
-** Login   <delafob@epitech.net>
-** 
-** Started on  Mon Mar 11 13:23:46 2013 Brieuc de La Fouchardière
-** Last update Sun Mar 24 17:49:24 2013 Brieuc de La Fouchardière
-*/
-
 #ifndef SERV_H_
 # define SERV_H_
 
